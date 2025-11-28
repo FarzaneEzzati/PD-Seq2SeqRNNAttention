dcbb0ef64c27b5e2474ff47466c3a523b5777220
Farzane <ezzati.f.111@gmail.com>
2025-11-28 16:19:24 -0600

    Updated

--- Bahdanau-Attention-in-Pytorch-master/Attention.py
+++ Bahdanau-Attention-in-Pytorch-master/Attention.py
@@ -26,17 +26,27 @@ class Attention(nn.Module):
                 att_weights:    [batch_size, src_seq_len]
             Important: in each time step of decoder the attention weights are calculated.
         '''
-        
+        # Get batch size and source length
+        # This step aligns the enc_lasy_layer hidden states shape with the same shape for dec_last_layer hidden states
         batch_size = hidden_per_time.size(0)
         src_seq_len = hidden_per_time.size(1)  # the length of input
-        # This step aligns the enc_lasy_layer hidden states shape with the same shape for dec_last_layer hidden states
+        
+        # Use only the last decoder hidden state
+        # Repeat decoder hidden across all source positions
         dec_hidden = dec_hidden[:, -1, :].unsqueeze(1).repeat(1, src_seq_len, 1)  # [batch_size, src_seq_len, dec_hidden_dim]
+        
+        # Compute energy transformation
         tanh_W_s_h = torch.tanh(self.W(torch.cat((dec_hidden, hidden_per_time), dim=2)))  # [batch_size, src_seq_len, dec_hidden_dim]
+        
+        # Prepare for dot-product with vector v
         tanh_W_s_h = tanh_W_s_h.permute(0, 2, 1)       # [batch_size, dec_hidden_dim, seq_len]
         
+        # Repeat learnable vector v for each batch
         V = self.V.repeat(batch_size, 1).unsqueeze(1)  # [batch_size, 1, dec_hidden_dim]
-        e = torch.bmm(V, tanh_W_s_h).squeeze(1)        # [batch_size, seq_len]
         
+        # Compute unnormalized attention scores and get softmax
+        e = torch.bmm(V, tanh_W_s_h).squeeze(1)        # [batch_size, seq_len]
         att_weights = F.softmax(e, dim=1)              # [batch_size, src_seq_len]
+        
         return att_weights  # [batch_size, src_seq_len]
     
